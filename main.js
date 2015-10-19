467423a49d791a942cc1ad5fe78f4d4c36fb1f53
chrisdavidmills <chrisdavidmills@gmail.com>
2015-10-19 10:57:51 +0100

    base-64 encoding key in other instance of subscribeObj

--- main.js
+++ main.js
@@ -234,7 +234,7 @@ function updateStatus(endpoint,key,statusType) {
                          statusType: statusType,
                          name: nameInput.value,
                          endpoint: endpoint,
-                         key: key
+                         key: btoa(String.fromCharCode.apply(null, new Uint8Array(key)))
                        }
     console.log(subscribeObj);
     request.send(JSON.stringify(subscribeObj));
